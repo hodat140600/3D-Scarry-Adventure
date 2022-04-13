1ca345a70554dbefa8f251540c9908436f6ff42f
hodat140600 <hodat140600@gmail.com>
2022-04-13 15:50:06 +0700

    First commit

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class GameEnding : MonoBehaviour
+{
+    public float faderDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+    public float displayImageDuration = 1f;
+
+    bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught = false;
+    bool m_HasAudioPlayed = false;
+    float m_Timer;
+
+    private void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught) {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player) {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!m_HasAudioPlayed) {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+        //Debug.Log(imageCanvasGroup);
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / faderDuration;
+        if (m_Timer > faderDuration + displayImageDuration) {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void CaughtPlayer() {
+        m_IsPlayerCaught = true;
+    }
+}
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    public GameEnding gameEnding;
+
+    bool m_IsPlayerInRange;
+
+    private void Update()
+    {
+        if (m_IsPlayerInRange) {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            RaycastHit rayCastHit;
+            Ray ray = new Ray(transform.position, direction);
+            if (Physics.Raycast(ray, out rayCastHit)) {
+                if (rayCastHit.collider.transform == player) {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player) {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+}
